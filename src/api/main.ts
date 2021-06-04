67221662f92b9b6ea78503e6a1a2ab8d3cce6af7
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-06-04 15:06:42 -0300

    remove teste

--- src/api/main.ts
+++ src/api/main.ts
@@ -9,10 +9,6 @@ const app = createExpressServer({
   ],
 });
 
-app.use('/', (req, res) => {
-  res.send('ok')
-})
-
 const PORT = process.env.PORT ? process.env.PORT : 3000
 
 app.listen(3000, () => {
